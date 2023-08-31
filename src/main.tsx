4fafb80e9ce734685c68080cf6e9ef5f2cace0d0
msm <myatsumon@ucsy.edu.mm>
2023-08-31 07:14:07 +0630

    usereducer and enum type

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App3";
+import App from "./App4";
 import { StateContextProvider } from "./context/StateContext";
 import "./App.css";
 
